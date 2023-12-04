e65f17d8781b5b7619124e63c7659dd922c97098
Haris <6013429@mborijnland.nl>
2023-12-04 09:48:18 +0100

    added register() method to User class

--- classes/User.php
+++ classes/User.php
@@ -3,4 +3,28 @@
 class User extends DbConfig
 {
 
+    private $username;
+    private $password;
+    public function register($data)
+    {
+        try {
+
+            $this->username = $data['username'];
+            $this->password = $data['password'];
+            $hashed_password = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO users(username, password) VALUES(:username, :password)";
+            $stmt = self::connect()->prepare($sql);
+            $stmt->bindParam(':username', $this->username);
+            $stmt->bindParam(':password', $hashed_password);
+
+            if (!$stmt->execute()) {
+                throw new Exception("Error, registratie mislukt.");
+            }
+
+            return header('Location: home.php');
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 }
