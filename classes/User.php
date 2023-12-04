e5001713c578b0044bb49e4ea8fa06b5d7711718
Haris <6013429@mborijnland.nl>
2023-12-04 09:48:52 +0100

    added login() method to User class

--- classes/User.php
+++ classes/User.php
@@ -27,4 +27,36 @@ class User extends DbConfig
             return $e->getMessage();
         }
     }
+
+    public function login($data)
+    {
+        try {
+            $this->username = $data['username'];
+            $this->password = $data['password'];
+
+            $sql = "SELECT * FROM users WHERE username = :username LIMIT 1";
+
+            $stmt = self::connect()->prepare($sql);
+            $stmt->bindParam(':username', $this->username);
+
+            if (!$stmt->execute()) {
+                throw new PDOException("Login mislukt.");
+            }
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($stmt->rowCount() == 1) {
+                $hashpassword = $row['password'];
+                if (password_verify($this->password, $hashpassword)) {
+                    $_SESSION['user'] = $row;
+                } else {
+                    return false;
+                }
+
+                return header('Location: home.php');
+            }
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
 }
