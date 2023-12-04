057c7bdebc268706d7896af9bda192677b4cca95
Haris <6013429@mborijnland.nl>
2023-12-04 09:52:46 +0100

    added update() method to User class

--- classes/User.php
+++ classes/User.php
@@ -69,40 +69,66 @@ class User extends DbConfig
     }
 
     public function read($id)
-    { 
-            try {
-                $sql = "SELECT * FROM users WHERE id = :id";
+    {
+        try {
+            $sql = "SELECT * FROM users WHERE id = :id";
 
-                $stmt = self::connect()->prepare($sql);
-                $stmt->bindParam(':id', $id);
+            $stmt = self::connect()->prepare($sql);
+            $stmt->bindParam(':id', $id);
 
-                if (!$stmt->execute()) {
-                    throw new PDOException("read mislukt..");
-                }
-
-                $result = $stmt->fetch();
-                return $result;
-            } catch (Exception $e) {
-                $e->getMessage();
+            if (!$stmt->execute()) {
+                throw new PDOException("read mislukt..");
             }
+
+            $result = $stmt->fetch();
+            return $result;
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
     }
 
     public static function readAll()
-    { {
-            try {
-                $sql = "SELECT * FROM users";
+    {
+        try {
+            $sql = "SELECT * FROM users";
 
-                $stmt = self::connect()->prepare($sql);
-                if (!$stmt->execute()) {
-                    throw new PDOException("read mislukt..");
-                }
-
-                $result = $stmt->fetchAll();
-                return $result;
-            } catch (Exception $e) {
-                $e->getMessage();
+            $stmt = self::connect()->prepare($sql);
+            if (!$stmt->execute()) {
+                throw new PDOException("read mislukt..");
             }
+
+            $result = $stmt->fetchAll();
+            return $result;
+        } catch (Exception $e) {
+            $e->getMessage();
+        }
+    }
+
+    public function update($data, $id)
+    {
+        try {
+
+            $this->username = $data['username'];
+            $this->password = $data['password'];
+            $hashed_password = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $sql = "UPDATE users SET 
+                 username = :username,
+                 password = :password, 
+                 WHERE id = :id";
+
+            $stmt = self::connect()->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->bindParam(':username', $this->username);
+            $stmt->bindParam(':password', $hashed_password);
+
+            if (!$stmt->execute()) {
+                throw new Exception("Error, user toevoegen mislukt.");
+            }
+
+            return header('Location: index.php');
+        } catch (Exception $e) {
+            return $e->getMessage();
         }
     }
 }
-
