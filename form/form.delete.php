c6b36c0acf4e8bfbf331337932d69c13758adcad
Haris <6013429@mborijnland.nl>
2023-12-04 11:34:49 +0100

    added delete form for wishes

---
+++ form/form.delete.php
@@ -0,0 +1,29 @@
+<?php 
+require_once '../classes/DbConfig.php';
+require_once '../classes/Wish.php';
+
+if (!DbConfig::getSession()) {
+    header('Location: login.php');
+}
+
+$wish = new Wish();
+
+if (isset($_POST['submit'])) {
+    $wish->delete($_GET['delete']);
+    header('Location: ../wishes.php');
+}
+
+if (isset($_GET['back'])) {
+    header('Location: ../../wishes.php');
+}
+
+?>
+<?php require_once '../includes/header.php'; ?>
+<main class="container mt-5">
+    <form action="" method="POST">
+        <h1 class="fw-bold text-center">Confirm</h1>
+        <button class="w-100 btn btn-lg btn-danger" type="submit" name="submit">Delete</button>
+        <a href="?back" class="w-100 btn btn-lg btn-outline-primary my-2">Cancel</a>
+    </form>
+</main>
+<?php require_once '../includes/footer.php'; ?>
